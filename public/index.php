7e8634e13e67fd73924a7074a684a05bbb0ff1d2
kentkentj <kentkentj@gmail.com>
2021-11-15 11:24:32 +0800

    PHP Create and Read

---
+++ public/index.php
@@ -0,0 +1,14 @@
+<?php include 'templates/header.php'; ?>
+ <ul>
+	<li>
+		<strong>
+			<a href="create.php">Create - add a user</a>
+		</strong>
+	</li>
+	<li>
+		<strong>
+			<a href="read.php">Read - find a user</a>
+		</strong>
+	</li>
+</ul>
+<?php include 'templates/footer.php'; ?>
---
+++ public/templates/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8"/>
+	<meta http-equiv="x-ua-compatible" content="ie-edge">
+	<meta name="viewport" content="width=device">
+	<meta charset="utf-8">
+	<title>Php Intro</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<h1>Simple Database App</h1>
